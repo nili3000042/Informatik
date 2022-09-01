d9ed5a349c893dd00ce323845010c383d4441912
nili3000042 <nili3000042@gmail.com>
2022-09-01 13:04:40 +0200

    Stunde

---
+++ Q1/_Wiederholungen/Arrays/Wuerfel.java
@@ -0,0 +1,21 @@
+import java.util.*;
+/**
+ * Beschreiben Sie hier die Klasse Scanner.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Wuerfel
+{
+    Scanner scan = new Scanner(System.in);
+    Random rnd = new Random();
+    //String eingabe = scan.nextLine();
+    //double eingabe = scan.nextDouble();
+    //char eingabe = scan.next().charAt(0);
+    //System.out.println("Was soll ich Rechnen?");
+    //System.out.println('\u000c');
+    public static void wuerfel(int anzahl, int max)
+    {
+        
+    }
+}
