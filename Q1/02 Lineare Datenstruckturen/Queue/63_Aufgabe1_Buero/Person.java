495ff2b28cc8d8f3db567be4032c4329483d3af7
nili3000042 <nili3000042@gmail.com>
2022-11-17 13:09:17 +0100

    Altzer

--- Q1/02 Lineare Datenstruckturen/Queue/63_Aufgabe1_Buero/Person.java
+++ Q1/02 Lineare Datenstruckturen/Queue/63_Aufgabe1_Buero/Person.java
@@ -2,14 +2,21 @@ public class Person
 {
     private String name;
     private Person nachfolger;
+    private int alter;
 
-    public Person(String pName)
+    public Person(String pName, int pAlter)
     {
         name = pName;    
+        alter = pAlter;
     }
 
     public String getName()
     {
         return name;
+    }
+    
+    public int getAlter()
+    {
+        return alter;
     }
 }
